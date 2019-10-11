6e9aab46349326a144c3fa3a0308dc1a0172e52b
Aaron M <Boomslangnz@hotmail.com>
2019-10-11 17:22:28 +1300

    rename FFBPlugin.txt to FFBPluginReadme.txt
    cos people never know its a readme lol

--- Post-Build/copydlls.bat
+++ Post-Build/copydlls.bat
@@ -1,5 +1,5 @@
 cd %1 %2
 for /D /r %%d in (.) do @xcopy *.dll "%%d\" /y
-for /D /r %%d in (.) do @xcopy FFBPlugin.txt "%%d\" /y
+for /D /r %%d in (.) do @xcopy FFBPluginReadme.txt "%%d\" /y
 del %1\*.dll
-del %1\FFBPlugin.txt
+del %1\FFBPluginReadme.txt
