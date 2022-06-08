82ff1c0998122bae1d329d19242813956c006275
Boomslangnz <Boomslangnz@hotmail.com>
2022-06-08 17:07:52 +1200

    Oops

--- Game Files/WMMT3.cpp
+++ Game Files/WMMT3.cpp
@@ -59,13 +59,12 @@ void WMMT3::FFBLoop(EffectConstants* constants, Helpers* helpers, EffectTriggers
 
 	if (setReflect != OldsetReflect)
 	{
-		//if (setReflect == 0x00)
-		//{
-		//	triggers->Constant(constants->DIRECTION_FROM_LEFT, 0);
-		//	triggers->Constant(constants->DIRECTION_FROM_RIGHT, 0);
-		//}
-		//else 
-		if (setReflect > 0x00 && setReflect <= 0x3F)
+		if (setReflect == 0x00)
+		{
+			triggers->Constant(constants->DIRECTION_FROM_LEFT, 0);
+			triggers->Constant(constants->DIRECTION_FROM_RIGHT, 0);
+		}
+		else if (setReflect > 0x00 && setReflect <= 0x3F)
 		{
 			double percentForce = setReflect / 63.0;
 			double percentLength = 100;
@@ -83,14 +82,13 @@ void WMMT3::FFBLoop(EffectConstants* constants, Helpers* helpers, EffectTriggers
 
 	if (setCenterOffset != OldsetCenterOffset)
 	{
-/*		if (setCenterOffset == 0x00)
+		if (setCenterOffset == 0x00)
 		{
 			triggers->Rumble(0, 0, 0);
 			triggers->Constant(constants->DIRECTION_FROM_LEFT, 0);
 			triggers->Constant(constants->DIRECTION_FROM_RIGHT, 0);
 		}
-		else*/ 
-		if (setCenterOffset > 0x00 && setCenterOffset <= 0x3F)
+		else if (setCenterOffset > 0x00 && setCenterOffset <= 0x3F)
 		{
 			double percentForce = setCenterOffset / 63.0;
 			double percentLength = 100;
