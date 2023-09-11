cdf4a0850e34e8201ba53b1e2316e5893a2d8835
Boomslangnz <Boomslangnz@hotmail.com>
2023-09-11 19:10:50 +1200

    Update SDL2 and tweak init

--- DllMain.cpp
+++ DllMain.cpp
@@ -1075,9 +1075,7 @@ void Initialize(int device_index)
 {
 	hlp.log("in initialize");
 	SDL_SetHint(SDL_HINT_JOYSTICK_RAWINPUT, "0");
-	SDL_Init(SDL_INIT_HAPTIC | SDL_INIT_SENSOR);
-	if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) < 0);
-	if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) < 0);
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC | SDL_INIT_GAMECONTROLLER) < 0)
 	SDL_JoystickEventState(SDL_ENABLE);
 	SDL_JoystickUpdate();
 	char joystick_guid[256];
