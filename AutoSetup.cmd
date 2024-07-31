f4db76fe7bc607573d2d3249bf2cbe3e28be002d
Boomslangnz <Boomslangnz@hotmail.com>
2024-08-01 11:09:19 +1200

    AutoSetup

--- AutoSetup.cmd
+++ AutoSetup.cmd
@@ -136,6 +136,9 @@ cd..
 cd R-Tuned
 rename dinput8.dll opengl32.dll
 cd..
+cd "Sega Race TV"
+rename dinput8.dll opengl32.dll
+cd..
 cd "Sega World Drivers Championship 2018"
 rename dinput8.dll d3d11.dll
 cd..
